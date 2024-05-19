4da6c43ae8eabe38e5bcc6059cef41207a68b26d
clvieira_microsoft <Claudio.Merola@microsoft.com>
2024-05-19 17:32:46 -0300

    Improvements on VM Module

--- Modules/Compute/VM.ps1
+++ Modules/Compute/VM.ps1
@@ -13,7 +13,7 @@ https://github.com/microsoft/ARI/Modules/Compute/VM.ps1
 This powershell Module is part of Azure Resource Inventory (ARI)
 
 .NOTES
-Version: 3.1.5
+Version: 3.1.6
 First Release Date: 19th November, 2020
 Authors: Claudio Merola and Renato Gregio 
 
@@ -30,6 +30,7 @@ If ($Task -eq 'Processing')
         $nic = $Resources | Where-Object {$_.TYPE -eq 'microsoft.network/networkinterfaces'}
         $vmexp = $Resources | Where-Object {$_.TYPE -eq 'microsoft.compute/virtualmachines/extensions'}
         $disk = $Resources | Where-Object {$_.TYPE -eq 'microsoft.compute/disks'}
+        $VirtualNetwork = $Resources | Where-Object { $_.TYPE -eq 'microsoft.network/virtualnetworks' }
         $vmsizemap = @{}
         foreach($location in ($vm | Select-Object -ExpandProperty location -Unique))
             {
@@ -50,14 +51,14 @@ If ($Task -eq 'Processing')
                 {
                     $ResUCount = 1
                     $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
-                    $data = $1.PROPERTIES 
+                    $data = $1.PROPERTIES
                     $timecreated = $data.timeCreated
                     $timecreated = [datetime]$timecreated
                     $timecreated = $timecreated.ToString("yyyy-MM-dd HH:mm")
-                    $AVSET = ''
                     $dataSize = ''
                     $StorAcc = ''
 
+                    #Retirements Validation
                     $RetDate = ''
                     $RetFeature = '' 
                     if($data.hardwareProfile.vmSize -in ('basic_a0','basic_a1','basic_a2','basic_a3','basic_a4','standard_a0','standard_a1','standard_a2','standard_a3','standard_a4','standard_a5','standard_a6','standard_a7','standard_a9') -or $1.sku.name -in ('basic_a0','basic_a1','basic_a2','basic_a3','basic_a4','standard_a0','standard_a1','standard_a2','standard_a3','standard_a4','standard_a5','standard_a6','standard_a7','standard_a9'))
@@ -95,9 +96,8 @@ If ($Task -eq 'Processing')
                             $RetDate = ($Unsupported | Where-Object {$_.Id -eq 4}).RetirementDate
                             $RetFeature = ($Unsupported | Where-Object {$_.Id -eq 4}).RetiringFeature
                         }
-                    
-                    $UpdateMgmt = if ($null -eq $data.osProfile.LinuxConfiguration.patchSettings.patchMode) { $data.osProfile.WindowsConfiguration.patchSettings.patchMode } else { $data.osProfile.LinuxConfiguration.patchSettings.patchMode }                    
 
+                    #Extensions 
                     $ext = @()
                     $AzDiag = ''
                     $Azinsights = ''
@@ -122,8 +122,33 @@ If ($Task -eq 'Processing')
                             $ext = $ext.Substring(0, $ext.Length - 2)
                         }
 
-                    if ($null -ne $data.availabilitySet) { $AVSET = 'True' }else { $AVSET = 'False' }
+                    if(![string]::IsNullOrEmpty($data.osprofile.windowsconfiguration.enableautomaticupdates))
+                        {
+                            if($data.osprofile.windowsconfiguration.enableautomaticupdates -eq 'True')
+                                {
+                                    $Autoupdate = $true
+                                }
+                            else
+                                {
+                                    $Autoupdate = $false
+                                }
+                        }
+                    elseif(![string]::IsNullOrEmpty($data.osprofile.linuxconfiguration.patchsettings.patchmode))
+                        {
+                            if($data.osprofile.linuxconfiguration.patchsettings.patchmode -eq 'automaticbyos')
+                                {
+                                    $Autoupdate = $true
+                                }
+                            else
+                                {
+                                    $Autoupdate = $false
+                                }
+                        }
+
+                    if (![string]::IsNullOrEmpty($data.availabilitySet)) { $AVSET = $true }else { $AVSET = $false }
                     if ($data.diagnosticsProfile.bootDiagnostics.enabled -eq $true) { $bootdg = $true }else { $bootdg = $false }
+
+                    #Storage
                     if($data.storageProfile.osDisk.managedDisk.id) 
                         {
                             $OSDisk = ($disk | Where-Object {$_.id -eq $data.storageProfile.osDisk.managedDisk.id} | Select-Object -Unique).sku.name
@@ -149,18 +174,52 @@ If ($Task -eq 'Processing')
                                 else 
                                 { 
                                     ($disk | Where-Object {$_.id -eq $data.storageProfile.dataDisks.managedDisk.id}).properties.diskSizeGB
-                                }                    
+                                }
+
                     $Tags = if(![string]::IsNullOrEmpty($1.tags.psobject.properties)){$1.tags.psobject.properties}else{'0'}
                     $VMNICS = if(![string]::IsNullOrEmpty($data.networkProfile.networkInterfaces.id)){$data.networkProfile.networkInterfaces.id}else{'0'}
                     foreach ($2 in $VMNICS) {
 
                         $vmnic = $nic | Where-Object { $_.ID -eq $2 } | Select-Object -Unique
-                        $vmnsg = if($vmnic.properties.networkSecurityGroup.id){$vmnic.properties.networkSecurityGroup.id.split('/')[8]}else{'None'}
-                        $PIP = $vmnic.properties.ipConfigurations.properties.publicIPAddress.id.split('/')[8]
-                        $VNET = $vmnic.properties.ipConfigurations.properties.subnet.id.split('/')[8]
-                        $Subnet = $vmnic.properties.ipConfigurations.properties.subnet.id.split('/')[10]
+                        $PIP = if(![string]::IsNullOrEmpty($vmnic.properties.ipConfigurations.properties.publicIPAddress.id)){$vmnic.properties.ipConfigurations.properties.publicIPAddress.id.split('/')[8]}else{''}
+                        $VNET = if(![string]::IsNullOrEmpty($vmnic.properties.ipConfigurations.properties.subnet.id)){$vmnic.properties.ipConfigurations.properties.subnet.id.split('/')[8]}else{''}
+                        $Subnet = if(![string]::IsNullOrEmpty($vmnic.properties.ipConfigurations.properties.subnet.id)){$vmnic.properties.ipConfigurations.properties.subnet.id.split('/')[10]} else {''}
+                        $vmnet = $VirtualNetwork | Where-Object {$_.properties.subnets.id -eq $vmnic.properties.ipConfigurations.properties.subnet.id}
+                        $vmnetsubnet = $vmnet.properties.subnets | Where-Object {$_.id -eq $vmnic.properties.ipConfigurations.properties.subnet.id}
 
+                        if(![string]::IsNullOrEmpty($vmnic.properties.dnsSettings.dnsServers))
+                            {
+                                $DNSServer = $vmnic.properties.dnsSettings.dnsServers
+                            }
+                        else
+                            {
+                                $DNSServer = $vmnet.properties.dhcpoptions.dnsservers
+                            }
 
+                        if(![string]::IsNullOrEmpty($DNSServer))
+                            {
+                                $FinalDNS = if ($DNSServer.count -gt 1) { $DNSServer | ForEach-Object { $_ + ' ,' } }else { $DNSServer }
+                                $FinalDNS = [string]$FinalDNS
+                                $FinalDNS = if ($FinalDNS -like '* ,*') { $FinalDNS -replace ".$" }else { $FinalDNS }
+                                $FinalDNS = ('VNET: ( ' + $FinalDNS + ')')
+                            }
+                        else
+                            {
+                                $FinalDNS = 'Default (Azure-provided)'
+                            }
+
+                        if(![string]::IsNullOrEmpty($vmnic.properties.networkSecurityGroup.id))
+                            {
+                                $vmnsg = $vmnic.properties.networkSecurityGroup.id.split('/')[8]
+                            }
+                        elseif(![string]::IsNullOrEmpty($vmnetsubnet.properties.networksecuritygroup.id))
+                            {
+                                $vmnsg = ('Subnet: ('+$vmnetsubnet.properties.networksecuritygroup.id.split('/')[8]+')')
+                            }
+                        else
+                            {
+                                $vmnsg = 'None'
+                            }
 
                         foreach ($Tag in $Tags) 
                             {
@@ -182,9 +241,9 @@ If ($Task -eq 'Processing')
                                 'OS Type'                       = $data.storageProfile.osDisk.osType;
                                 'OS Name'                       = $data.extended.instanceView.osname;
                                 'OS Version'                    = $data.extended.instanceView.osversion;
+                                'Automatic Update'              = $Autoupdate;
                                 'Retirement Date'               = [string]$RetDate;
                                 'Retirement Feature'            = $RetFeature;
-                                'Update Management'             = $UpdateMgmt;
                                 'Boot Diagnostics'              = $bootdg;
                                 'Performance Agent'             = if ($azDiag -ne '') { $true }else { $false };
                                 'Azure Monitor'                 = if ($Azinsights -ne '') { $true }else { $false };
@@ -192,10 +251,11 @@ If ($Task -eq 'Processing')
                                 'OS Disk Size (GB)'             = $OSDiskSize;
                                 'Data Disk Storage Type'        = $StorAcc;
                                 'Data Disk Size (GB)'           = $dataSize;
+                                'VM generation'                 = $data.extended.instanceview.hypervgeneration;
                                 'Power State'                   = $data.extended.instanceView.powerState.displayStatus;
                                 'NIC Name'                      = [string]$vmnic.name;
                                 'NIC Type'                      = [string]$vmnic.properties.nicType;
-                                'DNS Servers'                   = [string]$vmnic.properties.dnsSettings.dnsServers;
+                                'DNS Servers'                   = $FinalDNS;
                                 'Public IP'                     = $PIP;
                                 'Virtual Network'               = $VNET;
                                 'Subnet'                        = $Subnet;
@@ -213,11 +273,10 @@ If ($Task -eq 'Processing')
                                 $tmp += $obj
                                 if ($ResUCount -eq 1) { $ResUCount = 0 } 
                             }
-                            Remove-Variable PIP, vmnic, vmnsg, VNET, Subnet                        
                         }
                     }
                     $tmp
-        }            
+        }
 }
 else
 {
@@ -225,28 +284,26 @@ else
         {
             $TableName = ('VMTable_'+($SmaResources.VM.id | Select-Object -Unique).count)
             $Style = New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat '0' -VerticalAlignment Center
-            $StyleExt = New-ExcelStyle -HorizontalAlignment Left -Range AK:AK -Width 60 -WrapText 
+            $StyleExt = New-ExcelStyle -HorizontalAlignment Left -Range AN:AN -Width 60 -WrapText 
 
                 $cond = @()
+                #Automatic Updates
+                $cond += New-ConditionalText false -Range K:K
                 #Hybrid Benefit
-                $cond += New-ConditionalText None -Range O:O
-                #NSG
-                $cond += New-ConditionalText None -Range AE:AE
+                $cond += New-ConditionalText None -Range P:P
                 #Boot Diagnostics
-                $cond += New-ConditionalText falso -Range R:R
                 $cond += New-ConditionalText false -Range R:R
                 #Performance Agent
-                $cond += New-ConditionalText falso -Range S:S
                 $cond += New-ConditionalText false -Range S:S
                 #Azure Monitor
-                $cond += New-ConditionalText falso -Range T:T
                 $cond += New-ConditionalText false -Range T:T
+                #NSG
+                $cond += New-ConditionalText None -Range AF:AF
                 #Acelerated Network
-                $cond += New-ConditionalText false -Range AG:AG
-                $cond += New-ConditionalText falso -Range AG:AG  
+                $cond += New-ConditionalText false -Range AI:AI
                 #Retirement
-                $cond += New-ConditionalText - -Range M:M -ConditionalType ContainsText
-    
+                $cond += New-ConditionalText - -Range N:N -ConditionalType ContainsText
+
                 $Exc = New-Object System.Collections.Generic.List[System.Object]
                 $Exc.Add('Subscription')
                 $Exc.Add('Resource Group')
@@ -258,13 +315,13 @@ else
                 $Exc.Add('OS Type')
                 $Exc.Add('OS Name')
                 $Exc.Add('OS Version')
+                $Exc.Add('Automatic Update')
                 $Exc.Add('Image Reference')
                 $Exc.Add('Image Version')
                 $Exc.Add('Retirement Date')
                 $Exc.Add('Retirement Feature')
                 $Exc.Add('Hybrid Benefit')
                 $Exc.Add('Admin Username')
-                $Exc.Add('Update Management')
                 $Exc.Add('Boot Diagnostics')
                 $Exc.Add('Performance Agent')
                 $Exc.Add('Azure Monitor')
@@ -272,6 +329,7 @@ else
                 $Exc.Add('OS Disk Size (GB)')
                 $Exc.Add('Data Disk Storage Type')
                 $Exc.Add('Data Disk Size (GB)')
+                $Exc.Add('VM generation')
                 $Exc.Add('Power State')
                 $Exc.Add('Availability Set')
                 $Exc.Add('Zone')    
@@ -280,6 +338,7 @@ else
                 $Exc.Add('DNS Servers')
                 $Exc.Add('NSG')
                 $Exc.Add('NIC Name')
+                $Exc.Add('NIC Type')
                 $Exc.Add('Accelerated Networking')
                 $Exc.Add('IP Forwarding')
                 $Exc.Add('Private IP Address')
@@ -308,18 +367,18 @@ else
 
                 $excel = Open-ExcelPackage -Path $File -KillExcel
     
-                $null = $excel.'Virtual Machines'.Cells["M1"].AddComment("It's important to be aware of upcoming Azure services and feature retirements to understand their impact on your workloads and plan migration.", "Azure Resource Inventory")
-                $excel.'Virtual Machines'.Cells["M1"].Hyperlink = 'https://learn.microsoft.com/en-us/azure/advisor/advisor-how-to-plan-migration-workloads-service-retirement'
+                $null = $excel.'Virtual Machines'.Cells["N1"].AddComment("It's important to be aware of upcoming Azure services and feature retirements to understand their impact on your workloads and plan migration.", "Azure Resource Inventory")
+                $excel.'Virtual Machines'.Cells["N1"].Hyperlink = 'https://learn.microsoft.com/en-us/azure/advisor/advisor-how-to-plan-migration-workloads-service-retirement'
                 $null = $excel.'Virtual Machines'.Cells["R1"].AddComment("Boot diagnostics is a debugging feature for Azure virtual machines (VM) that allows diagnosis of VM boot failures.", "Azure Resource Inventory")
                 $excel.'Virtual Machines'.Cells["R1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/virtual-machines/boot-diagnostics'
                 $null = $excel.'Virtual Machines'.Cells["S1"].AddComment("Is recommended to install Performance Diagnostics Agent in every Azure Virtual Machine upfront. The agent is only used when triggered by the console and may save time in an event of performance struggling.", "Azure Resource Inventory")
                 $excel.'Virtual Machines'.Cells["S1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/virtual-machines/troubleshooting/performance-diagnostics'
                 $null = $excel.'Virtual Machines'.Cells["T1"].AddComment("We recommend that you use Azure Monitor to gain visibility into your resource's health.", "Azure Resource Inventory")
                 $excel.'Virtual Machines'.Cells["T1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/security/fundamentals/iaas#monitor-vm-performance'
-                $null = $excel.'Virtual Machines'.Cells["AE1"].AddComment("Use a network security group to protect against unsolicited traffic into Azure subnets. Network security groups are simple, stateful packet inspection devices that use the 5-tuple approach (source IP, source port, destination IP, destination port, and layer 4 protocol) to create allow/deny rules for network traffic.", "Azure Resource Inventory")
-                $excel.'Virtual Machines'.Cells["AE1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/security/fundamentals/network-best-practices#logically-segment-subnets'
-                $null = $excel.'Virtual Machines'.Cells["AG1"].AddComment("Accelerated networking enables single root I/O virtualization (SR-IOV) to a VM, greatly improving its networking performance. This high-performance path bypasses the host from the datapath, reducing latency, jitter, and CPU utilization.", "Azure Resource Inventory")
-                $excel.'Virtual Machines'.Cells["AG1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/virtual-network/create-vm-accelerated-networking-cli'
+                $null = $excel.'Virtual Machines'.Cells["AF1"].AddComment("Use a network security group to protect against unsolicited traffic into Azure subnets. Network security groups are simple, stateful packet inspection devices that use the 5-tuple approach (source IP, source port, destination IP, destination port, and layer 4 protocol) to create allow/deny rules for network traffic.", "Azure Resource Inventory")
+                $excel.'Virtual Machines'.Cells["AF1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/security/fundamentals/network-best-practices#logically-segment-subnets'
+                $null = $excel.'Virtual Machines'.Cells["AI1"].AddComment("Accelerated networking enables single root I/O virtualization (SR-IOV) to a VM, greatly improving its networking performance. This high-performance path bypasses the host from the datapath, reducing latency, jitter, and CPU utilization.", "Azure Resource Inventory")
+                $excel.'Virtual Machines'.Cells["AI1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/virtual-network/create-vm-accelerated-networking-cli'
 
             Close-ExcelPackage $excel
         }             
