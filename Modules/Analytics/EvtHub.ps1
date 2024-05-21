765ad6e7c3738e20c5fb9eee5460fff4426e5c2d
clvieira_microsoft <Claudio.Merola@microsoft.com>
2024-05-21 15:51:59 -0300

    Improvements on Event Hubs Module

--- Modules/Analytics/EvtHub.ps1
+++ Modules/Analytics/EvtHub.ps1
@@ -39,12 +39,11 @@ If ($Task -eq 'Processing')
                 $ResUCount = 1
                 $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
                 $data = $1.PROPERTIES
-                $timecreated = $data.createdAt
-                $timecreated = [datetime]$timecreated
-                $timecreated = $timecreated.ToString("yyyy-MM-dd HH:mm")
+                $timecreated = if($data.createdAt){[string](get-date($data.createdAt))}else{''}
                 $sku = $1.SKU
                 $RetDate = ''
-                $RetFeature = '' 
+                $RetFeature = ''
+                $LocalAuth = if($data.disablelocalauth -eq $false){$true}else{$false}
                 $Tags = if(![string]::IsNullOrEmpty($1.tags.psobject.properties)){$1.tags.psobject.properties}else{'0'}
                     foreach ($Tag in $Tags) { 
                         $obj = @{
@@ -59,9 +58,11 @@ If ($Task -eq 'Processing')
                             'Retirement Date'      = [string]$RetDate;
                             'Retirement Feature'   = $RetFeature;
                             'Throughput Units'     = $1.sku.capacity;
+                            'Local Authentication' = $LocalAuth;
                             'Auto-Inflate'         = $data.isAutoInflateEnabled;
                             'Max Throughput Units' = $data.maximumThroughputUnits;
                             'Kafka Enabled'        = $data.kafkaEnabled;
+                            'Minimum TLS Version'  = $data.minimumtlsversion;
                             'Endpoint'             = $data.serviceBusEndpoint;
                             'Created Time'         = $timecreated;
                             'Resource U'           = $ResUCount;
@@ -88,8 +89,8 @@ Else
         $Style = New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat '0'
 
         $condtxt = @()
-        $condtxt += New-ConditionalText false -Range J:J
-        $condtxt += New-ConditionalText falso -Range J:J
+        $condtxt += New-ConditionalText false -Range L:L
+        $condtxt += New-ConditionalText '1.0' -Range O:O
         $condtxt += New-ConditionalText - -Range H:H -ConditionalType ContainsText
 
         $Exc = New-Object System.Collections.Generic.List[System.Object]
@@ -103,9 +104,11 @@ Else
         $Exc.Add('Retirement Date')
         $Exc.Add('Retirement Feature')
         $Exc.Add('Throughput Units')
+        $Exc.Add('Local Authentication')
         $Exc.Add('Auto-Inflate')
         $Exc.Add('Max Throughput Units')
         $Exc.Add('Kafka Enabled')
+        $Exc.Add('Minimum TLS Version')
         $Exc.Add('Endpoint')
         $Exc.Add('Created Time')  
         if($InTag)
@@ -114,19 +117,22 @@ Else
                 $Exc.Add('Tag Value') 
             }
 
+        $noNumberConversion = @()
+        $noNumberConversion += 'Minimum TLS Version'
+
         $ExcelVar = $SmaResources.EvtHub  
 
         $ExcelVar | 
         ForEach-Object { [PSCustomObject]$_ } | Select-Object -Unique $Exc | 
-        Export-Excel -Path $File -WorksheetName 'Event Hubs' -AutoSize -MaxAutoSizeRows 100 -TableName $TableName -TableStyle $tableStyle -ConditionalText $condtxt -Style $Style, $StyleCost
+        Export-Excel -Path $File -WorksheetName 'Event Hubs' -AutoSize -MaxAutoSizeRows 100 -TableName $TableName -TableStyle $tableStyle -ConditionalText $condtxt -Style $Style -NoNumberConversion $noNumberConversion
 
         <######## Insert Column comments and documentations here following this model #########>
 
 
         $excel = Open-ExcelPackage -Path $File -KillExcel
 
-        $null = $excel.'Event Hubs'.Cells["J1"].AddComment("The Auto-inflate feature of Event Hubs automatically scales up by increasing the number of throughput units, to meet usage needs. Increasing throughput units prevents throttling scenarios.", "Azure Resource Inventory")
-        $excel.'Event Hubs'.Cells["J1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-auto-inflate'
+        $null = $excel.'Event Hubs'.Cells["L1"].AddComment("The Auto-inflate feature of Event Hubs automatically scales up by increasing the number of throughput units, to meet usage needs. Increasing throughput units prevents throttling scenarios.", "Azure Resource Inventory")
+        $excel.'Event Hubs'.Cells["L1"].Hyperlink = 'https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-auto-inflate'
 
         Close-ExcelPackage $excel 
     }
