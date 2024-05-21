029c276796635d3e1810034292c6409567bd90d4
clvieira_microsoft <Claudio.Merola@microsoft.com>
2024-05-21 13:52:20 -0300

    Improvements on Log Analytics Workspace Module

--- Modules/Analytics/WrkSpace.ps1
+++ Modules/Analytics/WrkSpace.ps1
@@ -42,26 +42,27 @@ If ($Task -eq 'Processing')
                 $data = $1.PROPERTIES
                 $RetDate = ''
                 $RetFeature = '' 
-                $timecreated = $data.createdDate
-                $timecreated = [datetime]$timecreated
-                $timecreated = $timecreated.ToString("yyyy-MM-dd HH:mm")
+                $timecreated = [string](get-date($data.createdDate))
+                $Quota = if($data.workspaceCapping.dailyQuotaGb -eq -1){'Off'}else{[string]$data.workspaceCapping.dailyQuotaGb}
                 $Tags = if(![string]::IsNullOrEmpty($1.tags.psobject.properties)){$1.tags.psobject.properties}else{'0'}
                     foreach ($Tag in $Tags) {
                         $obj = @{
-                            'ID'               = $1.id;
-                            'Subscription'     = $sub1.Name;
-                            'Resource Group'   = $1.RESOURCEGROUP;
-                            'Name'             = $1.NAME;
-                            'Location'         = $1.LOCATION;
-                            'Retirement Date'  = [string]$RetDate;
-                            'Retirement Feature' = $RetFeature;
-                            'SKU'              = $data.sku.name;
-                            'Retention Days'   = $data.retentionInDays;
-                            'Daily Quota (GB)' = [decimal]$data.workspaceCapping.dailyQuotaGb;
-                            'Created Time'     = $timecreated;
-                            'Resource U'       = $ResUCount;
-                            'Tag Name'         = [string]$Tag.Name;
-                            'Tag Value'        = [string]$Tag.Value
+                            'ID'                                        = $1.id;
+                            'Subscription'                              = $sub1.Name;
+                            'Resource Group'                            = $1.RESOURCEGROUP;
+                            'Name'                                      = $1.NAME;
+                            'Location'                                  = $1.LOCATION;
+                            'Retirement Date'                           = [string]$RetDate;
+                            'Retirement Feature'                        = $RetFeature;
+                            'SKU'                                       = $data.sku.name;
+                            'Retention Days'                            = $data.retentionInDays;
+                            'Daily Cap (GB)'                            = $Quota;
+                            'Data Ingestion From Public Networks'       = $data.publicNetworkAccessForIngestion;
+                            'Queries From Public Networks'              = $data.publicNetworkAccessForQuery;
+                            'Created Time'                              = $timecreated;
+                            'Resource U'                                = $ResUCount;
+                            'Tag Name'                                  = [string]$Tag.Name;
+                            'Tag Value'                                 = [string]$Tag.Value
                         }
                         $tmp += $obj
                         if ($ResUCount -eq 1) { $ResUCount = 0 } 
@@ -84,8 +85,10 @@ Else
         $Style = New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat '0.0'
 
         $condtxt = @()
-
         $condtxt += New-ConditionalText - -Range F:F -ConditionalType ContainsText
+        $condtxt += New-ConditionalText enabled -Range J:J
+        $condtxt += New-ConditionalText enabled -Range K:K
+        $condtxt += New-ConditionalText '0.' -Range I:I
 
         $Exc = New-Object System.Collections.Generic.List[System.Object]
         $Exc.Add('Subscription')
@@ -96,7 +99,9 @@ Else
         $Exc.Add('Retirement Date')
         $Exc.Add('Retirement Feature')
         $Exc.Add('Retention Days')
-        $Exc.Add('Daily Quota (GB)')
+        $Exc.Add('Daily Cap (GB)')
+        $Exc.Add('Data Ingestion From Public Networks')
+        $Exc.Add('Queries From Public Networks')
         $Exc.Add('Created Time')  
         if($InTag)
             {
@@ -104,11 +109,14 @@ Else
                 $Exc.Add('Tag Value') 
             }
 
+        $noNumberConversion = @()
+        $noNumberConversion += 'Daily Cap (GB)'
+
         $ExcelVar = $SmaResources.WrkSpace 
 
         $ExcelVar | 
         ForEach-Object { [PSCustomObject]$_ } | Select-Object -Unique $Exc | 
-        Export-Excel -Path $File -WorksheetName 'Workspaces' -AutoSize -MaxAutoSizeRows 100 -ConditionalText $condtxt -TableName $TableName -TableStyle $tableStyle -Style $Style
+        Export-Excel -Path $File -WorksheetName 'Workspaces' -AutoSize -MaxAutoSizeRows 100 -ConditionalText $condtxt -TableName $TableName -TableStyle $tableStyle -Style $Style -NoNumberConversion $noNumberConversion
 
 
         <######## Insert Column comments and documentations here following this model #########>
