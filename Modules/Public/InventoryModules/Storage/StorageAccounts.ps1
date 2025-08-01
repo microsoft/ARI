db88cc362341e3a63afede2f1d34993dd09f2fe3
Chad Schultz <25645350+itoleck@users.noreply.github.com>
2025-08-01 13:55:03 -0500

    Update to Storage Accounts collector add soft delete info

--- Modules/Public/InventoryModules/Storage/StorageAccounts.ps1
+++ Modules/Public/InventoryModules/Storage/StorageAccounts.ps1
@@ -79,6 +79,8 @@ If ($Task -eq 'Processing') {
                 $CrossTNT = if($data.allowCrossTenantReplication -eq $true){$true}else{$false}
                 $InfrastructureEncryption = if($data.encryption.requireInfrastructureEncryption -eq "True"){$true}else{$false}
 
+                
+
                 if ($data.azureFilesIdentityBasedAuthentication.directoryServiceOptions -eq 'None')
                     {
                         $EntraID = $false
@@ -128,6 +130,10 @@ If ($Task -eq 'Processing') {
                 $FinalACLIPs = [string]$FinalACLIPs
                 $FinalACLIPs = if ($FinalACLIPs -like '* ,*') { $FinalACLIPs -replace ".$" }else { $FinalACLIPs }
 
+                $sac = Get-AzStorageAccount -ResourceGroupName $1.RESOURCEGROUP -Name $1.NAME
+                $ctx=$sac.Context
+                $blobProperties = Get-AzStorageServiceProperty -ServiceType Blob -Context $ctx
+
                 foreach ($2 in $VNETRules)
                     {
                         $VNET = if(![string]::IsNullOrEmpty($2.id)){$2.id.split('/')[8]}else{''}
@@ -151,6 +157,7 @@ If ($Task -eq 'Processing') {
                                 'Microsoft Entra Authorization'         = $EntraID;
                                 'Allow Storage Account Key Access'      = $KeyAccess;
                                 'SFTP Enabled'                          = $SFTPEnabled;
+                                'Blob Soft Delete Days'                 = if ($blobProperties.DeleteRetentionPolicy.Enabled) { $blobProperties.DeleteRetentionPolicy.RetentionDays } else { 'N/A' };
                                 'Hierarchical Namespace'                = $HNSEnabled;
                                 'NFSv3 Enabled'                         = $NFSv3;
                                 'Large File Shares'                     = $LargeFileShare;
@@ -227,6 +234,7 @@ Else {
         $Exc.Add('Microsoft Entra Authorization')
         $Exc.Add('Allow Storage Account Key Access')
         $Exc.Add('SFTP Enabled')
+        $Exc.Add('Blob Soft Delete Days')
         $Exc.Add('Hierarchical Namespace')
         $Exc.Add('NFSv3 Enabled')
         $Exc.Add('Large File Shares')
